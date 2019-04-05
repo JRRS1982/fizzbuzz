4d42ad01e09ae1cd25d6b0219b6ac28267794c44
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-04-05 15:16:42 +0100

    ADD: add check to see if argument divisible by 3 and 5
    Add the ability for fizzbuzz to check if the argument passed is a 3 or 5, and
    return true if it is, else return false.

--- Javascript/spec/fizzBuzzSpec.js
+++ Javascript/spec/fizzBuzzSpec.js
@@ -6,20 +6,28 @@ describe('FizzBuzz', function() {
       fizzBuzz = new FizzBuzz();
       expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
     });
-    it('is divisible by five', function () {
+    it('is divisible by five', function() {
       fizzBuzz = new FizzBuzz();
       expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
     });
+    it('is divisible by three and five', function() {
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+    });
   });
 
-  describe('knowns when a number is not', function () {
-    it('is not divisible by three', function () {
+  describe('knowns when a number is not', function() {
+    it('is not divisible by three', function() {
       fizzBuzz = new FizzBuzz();
       expect(fizzBuzz.isDivisibleByThree(2)).toBe(false);
     });
-    it('is not divisible by five', function () {
+    it('is not divisible by five', function() {
       fizzBuzz = new FizzBuzz();
       expect(fizzBuzz.isDivisibleByFive(2)).toBe(false);
+    });
+    it('is not divisible by three and five', function() {
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByThreeAndFive(2)).toBe(false);
     });
   });
 });
--- Javascript/src/fizzBuzz.js
+++ Javascript/src/fizzBuzz.js
@@ -7,3 +7,7 @@ FizzBuzz.prototype.isDivisibleByThree = function(arg) {
 FizzBuzz.prototype.isDivisibleByFive = function(arg) { 
   return (arg % 5 === 0);
 };
+
+FizzBuzz.prototype.isDivisibleByThreeAndFive = function(arg) {
+  return (arg % 3 === 0 && arg % 5 === 0);
+};
