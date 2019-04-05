33087b121e509e9bcb03384d1b59a76835a1dff8
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-04-05 15:32:18 +0100

    REFACTOR: Add two arguments to the function, which removes the need for two functions.
    If two arguments are added to the function we can remove the two seperate functions,
    which also removes the hard coding of a number to the code and reduces the amount of
    code required. Seems sensible.

--- Javascript/spec/fizzBuzzSpec.js
+++ Javascript/spec/fizzBuzzSpec.js
@@ -6,10 +6,10 @@ describe('FizzBuzz', function() {
 
   describe('knowns when a number is', function() {
     it('is divisible by three', function() {
-      expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
+      expect(fizzBuzz._isDivisibleBy(3, 3)).toBe(true);
     });
     it('is divisible by five', function() {
-      expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+      expect(fizzBuzz._isDivisibleBy(5, 5)).toBe(true);
     });
     it('is divisible by three and five', function() {
       expect(fizzBuzz.isDivisibleByThreeAndFive(15)).toBe(true);
@@ -18,10 +18,10 @@ describe('FizzBuzz', function() {
 
   describe('knowns when a number is not', function() {
     it('is not divisible by three', function() {
-      expect(fizzBuzz.isDivisibleByThree(2)).toBe(false);
+      expect(fizzBuzz._isDivisibleBy(2, 3)).toBe(false);
     });
     it('is not divisible by five', function() {
-      expect(fizzBuzz.isDivisibleByFive(2)).toBe(false);
+      expect(fizzBuzz._isDivisibleBy(2, 5)).toBe(false);
     });
     it('is not divisible by three and five', function() {
       expect(fizzBuzz.isDivisibleByThreeAndFive(2)).toBe(false);
--- Javascript/src/fizzBuzz.js
+++ Javascript/src/fizzBuzz.js
@@ -1,13 +1,17 @@
 var FizzBuzz = function(){};
 
-FizzBuzz.prototype.isDivisibleByThree = function(arg) {
-  return (arg % 3 === 0);
-};
+// FizzBuzz.prototype.isDivisibleByThree = function(arg) {
+//   return (arg % 3 === 0);
+// };
 
-FizzBuzz.prototype.isDivisibleByFive = function(arg) { 
-  return (arg % 5 === 0);
-};
+// FizzBuzz.prototype.isDivisibleByFive = function(arg) { 
+//   return (arg % 5 === 0);
+// };
 
 FizzBuzz.prototype.isDivisibleByThreeAndFive = function(arg) {
   return (arg % 3 === 0 && arg % 5 === 0);
+};
+
+FizzBuzz.prototype._isDivisibleBy = function(number, divider) {
+  return (number % divider === 0)
 };
